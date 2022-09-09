b0086c81aee842964501429a46dd7ee31c4541ea
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-09 10:49:36 +0200

    create routes

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import{Route, BrowserRouter, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
