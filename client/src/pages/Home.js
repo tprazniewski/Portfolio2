29133e0ebae72a888e4935df07a3c64996b5e71f
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-16 10:42:58 +0200

    Start creating Home page and styling it

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import '../stylesheets/home.css'
+import React from "react";
+import "../stylesheets/home.css";
 
 function Home() {
   return (
-    <div className='introduction' style={{backgroundImage: `url(./bg.svg)`}}>
-
+    <div>
+      <div className="introduction flex-width-center"style={{ backgroundImage: `url(./bg.svg)` }}>
+        <div>
+          <h1>Tomasz</h1>
+          <div className="intro-content">
+            <p> Mern Stack <br /> Full Stack Developer </p>
+            <button className="primary-button font-bold"> Get Started </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
