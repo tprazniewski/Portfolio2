aac3c78b7f7c41d66a43f84b695c01ced899fcce
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-20 16:22:24 +0200

    test

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      <Header/>
+      <h1>hello</h1>
+    </div>
+
   )
 }
 
